e36fde703615309b26be2ca69aab3086d6650ac9
Andreza Feu <andrezafeu@gmail.com>
2018-04-06 12:24:13 -0400

    Register theme menus on functions.php

--- functions.php
+++ functions.php
@@ -2,6 +2,16 @@
 
 add_theme_support( 'menus' );
 
+function register_theme_menus() {
+  register_nav_menus(
+    array(
+      'primary-menu' => __( 'Primary Menu' )
+    )
+  );
+}
+// init tells wordpress to execute this when it initializes
+add_action ( 'init', 'register_theme_menus' );
+
 function wptf_theme_styles() {
   wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
   // wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
