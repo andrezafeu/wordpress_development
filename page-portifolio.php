0af4ef34756b38bcf0148805f1c9d68ce5f49fe1
Andreza Feu <andrezafeu@gmail.com>
2018-04-23 15:37:44 -0400

    Show posts of type 'my_portifolio'

--- page-portifolio.php
+++ page-portifolio.php
@@ -21,4 +21,22 @@
   </div>
 </section>
 
+<?php
+
+$args = array(
+  'post_type' => 'my_portifolio'
+);
+$query = new WP_Query( $args );
+
+?>
+
+<section class="row no-max pad">
+  <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+    <div class="small-6 medium-4 large-3 columns grid-item">
+      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a>
+    </div>
+  <!-- wp_reset_post_data is necessary if there are multiple loops in the page -->
+  <?php endwhile; endif; wp_reset_post_data(); ?>
+</section>
+
 <?php get_footer(); ?>
